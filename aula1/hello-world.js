4a56ddb8513a427fd07cb581d6ee3a606fffa3c0
Morvana Bonin <morvana.bonin@sensedia.com>
2020-09-27 19:54:23 -0300

    Exemplos de aula.

--- aula1/hello-world.js
+++ aula1/hello-world.js
@@ -1,4 +1,13 @@
-var hello = "Hello World";
+/*
+Isto é um exemplo simples em JavaScript
+*/
 
+//isto é uma constante, não muda.
+const HELLO = "Hello";
+
+//isto é uma variável, muda.
+let hello = HELLO + "World";
+
+//forma de impressão por console
 console.log(hello);
 
