47bd6682b1828b8233406bacb1be60018ab96a75
Morvana Bonin <morvana.bonin@sensedia.com>
2020-09-27 21:28:58 -0300

    Exemplos de aula.

---
+++ aula1/examplo-if.js
@@ -0,0 +1,13 @@
+//const média, essa não muda
+const MEDIA = 60
+
+/* 
+    variável com nota do estudante
+    muda de acordo cada estudante
+*/
+let notaEstudante = 75;
+
+//instrução de controle if
+if (notaEstudante >= MEDIA) {
+    console.log("Aprovado")
+}
