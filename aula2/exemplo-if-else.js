e9d3670682b49caef34c44c0a97e92d9ce29181c
Morvana Bonin <morvana.bonin@sensedia.com>
2020-10-04 15:08:30 -0300

    exemplo aula 2

---
+++ aula2/exemplo-if-else.js
@@ -0,0 +1,15 @@
+//const média, essa não muda
+const MEDIA = 60
+
+/* 
+    variável com nota do estudante
+    muda de acordo cada estudante
+*/
+let notaEstudante = 45;
+
+//instrução de controle if
+if (notaEstudante >= MEDIA) {
+    console.log("Aprovado")
+} else {
+    console.log("Reprovado")
+}
