0fde36bce8e3a0fe050defeffa4d04a9f784295b
Morvana Bonin <morvana.bonin@sensedia.com>
2020-10-04 23:15:53 -0300

    Exemplo switch 2.

--- aula2/exemplo-switch.js
+++ aula2/exemplo-switch.js
@@ -1,22 +1,19 @@
 /* 
     variável com nota do estudante
 */
-let nota = 95;
+let nota = "B";
 
 //instrução switch
 switch(nota) {
-    case nota >= 90:
-        console.log("A");
+    case "A":
+        console.log("Aluno aprovado com A");
       break;
-    case nota >= 80:
-        console.log("B");
+    case "B":
+        console.log("Aluno aprovado com B");
       break;
-    case nota >= 70:
-        console.log("C");
-      break;
-    case nota >= 60:
-        console.log("D");
+    case "C":
+        console.log("Aluno aprovado com C");
       break;
     default:
-        console.log("F");
+        console.log("Aluno reprovado");
 }
