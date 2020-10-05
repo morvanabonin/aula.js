835293adc4af6bf83ae60e52b93568d41f90444b
Morvana Bonin <morvana.bonin@sensedia.com>
2020-10-04 22:41:07 -0300

    Exemplo switch

---
+++ aula2/exemplo-switch.js
@@ -0,0 +1,23 @@
+//instrução switch
+switch (new Date().getDay()) {
+    case 0:
+      console.log("Sunday");
+      break;
+    case 1:
+      console.log("Monday");
+      break;
+    case 2:
+      console.log("Tuesday");
+      break;
+    case 3:
+      console.log("Wednesday");
+      break;
+    case 4:
+      console.log("Thursday");
+      break;
+    case 5:
+      console.log("Friday");
+      break;
+    case 6:
+      console.log("Saturday");
+  }
