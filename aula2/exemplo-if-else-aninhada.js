ddf86e3d6486d7e9338e351dac8bab88d51dda02
Morvana Bonin <morvana.bonin@sensedia.com>
2020-10-06 22:19:26 -0300

    WIP

---
+++ aula2/exemplo-if-else-aninhada.js
@@ -0,0 +1,18 @@
+/* 
+    variável com nota do estudante
+    muda de acordo cada estudante
+*/
+let notaEstudante = 85;
+
+//instrução de controle if
+if (notaEstudante >= 90) {
+    console.log("A");
+} else if (notaEstudante >= 80) {
+    console.log("B");
+} else if (notaEstudante >= 70) {
+    console.log("C");
+} else if (notaEstudante >= 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
