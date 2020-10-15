08277c054d75bf9becc4528fc9f6d5f072270cd3
Morvana Bonin <morvana.bonin@sensedia.com>
2020-10-14 21:51:21 -0300

    Exercicio while 3.

--- exercicios-while/exercicio2.js
+++ exercicios-while/exercicio2.js
@@ -1,8 +1,12 @@
+
 let i = 0;
 let anterior = 1;
 let posterior= 1;
 let atual=0;
-while (i < 30) {
+console.log(anterior);
+console.log(posterior);
+
+while (i < 28) {
     atual = anterior + posterior;
     anterior = posterior
     posterior = atual
---
+++ exercicios-while/exercicio3.js
@@ -0,0 +1,9 @@
+let i = 0;
+let num = 5;
+
+console.log("Tabuada do 5");
+while (i <= 10) {
+    let resultado = num * i;
+    console.log("5 x " + i + " = " + resultado );
+    i++;
+}
